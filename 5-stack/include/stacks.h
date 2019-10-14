3e79ede23e0bc9bcfd6191cb7c6294d31bbdb7b4
Oscar Olazabal <oscarolazabal.dev@gmail.com>
2019-10-14 14:36:31 -0700

    add stack problems

---
+++ 5-stack/include/stacks.h
@@ -0,0 +1,52 @@
+#ifndef STACKS
+#define STACKS
+
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+
+// For the following tasks manipulate the passed in stack.
+
+template <typename T>
+void push_elements(stack<T> &s, T arr[], size_t size) {
+    // Insert the values from the array into the stack
+    // ========= ONLY MODIFY BETWEEN THE LINES  ===========
+
+
+
+    // ====================================================
+}
+
+template <typename T>
+T sum(stack<T> &s) {
+    // Sum up all of the elements in the stack
+    // ========= ONLY MODIFY BETWEEN THE LINES  ===========
+    return 0;
+
+
+    // ====================================================
+}
+
+template <typename T>
+void clear(stack<T> &s) {
+    // Remove all elements from the stack
+    // ========= ONLY MODIFY BETWEEN THE LINES  ===========
+
+
+
+    // ====================================================
+}
+
+template <typename T>
+void reverse(stack<T> &s) {
+    // Reverse the elements of the stack
+    // ========= ONLY MODIFY BETWEEN THE LINES  ===========
+
+
+
+    // ====================================================
+}
+
+#endif
+
